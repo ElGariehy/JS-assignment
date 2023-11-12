03b791508582d34ec5a6e2a6e35fc55bf04787b3
ElGariehy Emad <70602010+ElGariehy@users.noreply.github.com>
2023-11-12 21:41:05 +0200

    challange in 17

---
+++ challange in 17/main.js
@@ -0,0 +1,8 @@
+let theTitle = "Elzero", theDesc ="Elzero Web School", theDate="25/10";
+
+let card= `<div class="card">
+    <h3>${theTitle}</h3>
+    <p>${theDesc}</p>
+    <span>${theDate}</span>
+</div>`;
+document.write(card.repeat(4));
