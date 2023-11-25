7b8e51fc2caab6d795a883a907a56a92d942fba5
ElGariehy Emad <70602010+ElGariehy@users.noreply.github.com>
2023-11-25 23:54:17 +0200

    assign 18-22

---
+++ assign 18-22/main.js
@@ -0,0 +1,68 @@
+
+              /*  التكليف 01    */
+
+// Replace ? With Arithmetic Operators
+console.log(10 * 20 + 15 % 3 + 190 + 10 - 400); // 0
+
+
+              /*  التكليف 02    */
+let num = 3;
+
+// Solution One
+console.log(num + num); // 6
+
+// Solution Two
+console.log(num  * true + true + true + true); // 6
+
+// Soultion Three
+console.log(num  * (true + true)); // 6
+
+// Soultion Four
+console.log(--num + ++num + true); // 6
+
+// Solution Five
+console.log(--num * ++num); // 6
+
+// Solution Six
+console.log(num + num - false); // 6
+
+
+
+              /*  التكليف 3    */
+
+let number = "10";
+
+// Solution One
+console.log(+number + +number); // 20
+
+// Solution Two
+console.log(+number * (true+true)); // 20
+
+// Solution Three
+console.log((+number / true) + +number); // 20
+
+// Solution Four
+console.log(--number + ++number +true); // 20
+
+              /*  التكليف 4    */
+
+let points = 10;
+
+// Write Your Code Here
+
+console.log(++points + true + true ); // 13
+
+// Write Your Code Here
+
+console.log(--points -true - true); // 8;
+
+
+
+
+
+
+
+
+
+
+
